760197aee6b5ccc34f456153487951d1027f1986
wazidu1 <wazid.ullah@dsinnovators.com>
2025-10-12 13:12:17 +0600

    Login - Logout Demo

--- tests/Codegen_Record_DemoLogin.spec.js
+++ tests/Codegen_Record_DemoLogin.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.locator('body').click();
+  //await page.locator('body').click();
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('visual_user');
